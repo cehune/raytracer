5a3c2683ec8d33e9ee74f9b1fd8d82539a20d6bc
mcka <mckale.chung@gmail.com>
2025-01-20 20:46:11 -0500

    Add camera class, clean up source.cpp main

--- src/Source.cpp
+++ src/Source.cpp
@@ -3,57 +3,16 @@
 #include "include/primitive_shapes/hittable.h"
 #include "include/primitive_shapes/hittable_list.h"
 #include "include/primitive_shapes/sphere.h"
-
-color ray_color(const ray& r, const hittable& world) {
-    hit_record rec;
-    if (world.hit(r, interval(0, infinity), rec)) { // currently tmin and tmax are 0, infinity, so no distance lim
-        return 0.5 * (rec.normal + color(1,1,1));
-    }
-    vec3 unit_direction = (r.direction()).normal_of();
-    auto a = 0.5*(unit_direction.y + 1.0);
-    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
-}
+#include "include/camera.h"
 
 int main() {
-
-    // Image
-    auto aspect_ratio = 16.0 / 9.0;
-    int image_width = 400;
-    // Calculate the image height, and ensure that it's at least 1.
-    int image_height = int(image_width / aspect_ratio);
-    image_height = (image_height < 1) ? 1 : image_height;
-
     // World
-
     hittable_list world;
     world.add(make_shared<sphere>(vec3(0,0,-1), 0.5));
     world.add(make_shared<sphere>(vec3(0,-100.5,-1), 100));
 
-    // Camera
-    auto focal_length = 1.0;
-    auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * (double(image_width)/image_height);
-    auto camera_center = vec3(0,0,0);
-
-    auto viewport_u = vec3(viewport_width, 0, 0);
-    auto viewport_v = vec3(0, -viewport_height, 0);
-    auto pixel_delta_u = viewport_u / image_width;
-    auto pixel_delta_v = viewport_v / image_height;
-    auto viewport_upper_left = camera_center - vec3(0,0,focal_length) - viewport_u / 2 - viewport_v / 2;
-    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
-    for (int j = 0; j < image_height; ++j) {
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-        for (int i = 0; i < image_width; i++) {
-            auto pixel_center = pixel00_loc + ((float)i * pixel_delta_u) + ((float)j * pixel_delta_v);
-            auto ray_direction = pixel_center - camera_center;
-            ray r(camera_center, ray_direction);
-            color pixel_color = ray_color(r, world);
-            write_color(std::cout, pixel_color);
-        }
-    }
-
-    std::clog << "\rDone.                 \n";
+    camera cam = camera();
+    cam.set_aspect_ratio(16.0 / 9.0);
+    cam.set_img_width(400);
+    cam.render(world);
 }
