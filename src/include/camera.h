0ced313996d08e2327cce209178b59796688ea3c
mcka <mckale.chung@gmail.com>
2025-01-21 21:21:18 -0500

    Add diffuse material reflection (ray reflection)

--- src/include/camera.h
+++ src/include/camera.h
@@ -13,6 +13,7 @@ private:
     int aa_samples_per_px;
     int image_width;  // Rendered image width in pixel count
     int image_height;   // Rendered image height
+    int ray_bounces; // four by default
     point3 center;         // Camera center
     point3 pixel00_loc;    // Location of pixel 0, 0
     vec3   pixel_delta_u;  // Offset to pixel to the right
@@ -40,11 +41,20 @@ private:
             center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
     }
-    color ray_color(const ray& r, const hittable& world) const {
+    color ray_color(const ray& r, int depth, const hittable& world) const {
         hit_record rec;
 
-        if (world.hit(r, interval(0, infinity), rec)) {
-            return 0.5 * (rec.normal + color(1,1,1));
+        if (depth <= 0) {
+            return color(0,0,0);
+        }
+
+        // set interval start at 0.001 to prevent a ray from bouncing with it's start surface due to float roundoff
+        if (world.hit(r, interval(0.001, infinity), rec)) {
+            //I think we need to put this reflective behaviour based on the material property of the element itself
+            // This is semi lambertian, where we are basing new ray direction on the normal, but not true
+            // Scatters rays towards the normals, but randomly
+            vec3 direction = rec.normal + random_vec_on_hemisphere(rec.normal);
+            return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);
         }
 
         vec3 normal_dir = (r.direction().normal_of());
@@ -53,13 +63,14 @@ private:
     }
 
 public:
-    camera(double aspect_rat, int img_width, vec3& c, double focal_len, double viewpt_height, int samples_per_px):
+    camera(double aspect_rat, int img_width, vec3& c, double focal_len, double viewpt_height, int samples_per_px,
+        int max_ray_bounces):
         aspect_ratio(aspect_rat), image_width(img_width), center(c), focal_length(focal_len),
-        viewport_height(viewpt_height), aa_samples_per_px(samples_per_px) {};
+        viewport_height(viewpt_height), aa_samples_per_px(samples_per_px), ray_bounces(max_ray_bounces) {};
     
     camera():
         aspect_ratio(1.0), image_width(400), center(vec3(0,0,0)), focal_length(1.0),
-        viewport_height(2.0), aa_samples_per_px(10) {};
+        viewport_height(2.0), aa_samples_per_px(10), ray_bounces(4) {};
 
     void set_aspect_ratio(double ratio) {aspect_ratio = ratio;}
     void set_img_width(int w) {image_width = w;}
@@ -78,7 +89,7 @@ public:
 
                 for (int s = 0; s < aa_samples_per_px; s++) {
                     ray offset_ray = generate_offset_ray(i, j, s);
-                    pixel_color += ray_color(offset_ray, world);
+                    pixel_color += ray_color(offset_ray, ray_bounces, world);
                 }
                 write_color(std::cout, pixel_samples_scale * pixel_color);
             }
