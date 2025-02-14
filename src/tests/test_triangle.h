6768dd0d3de336eead9ac7f6294b6d557c31daf5
mcka <mckale.chung@gmail.com>
2025-02-13 21:44:13 -0500

    Update trianglemesh to use buffer for memory efficiency

--- src/tests/test_triangle.h
+++ src/tests/test_triangle.h
@@ -7,7 +7,11 @@ void test_area() {
     vec3h p0(0.0, 0.0, 0.0, 1);
     vec3h p1(1.0, 0.0, 0.0, 1);
     vec3h p2(0.0, 1.0, 0.0, 1);
-    triangle tri(p0, p1, p2);
+
+    std::vector<vec3h> tri_vertices = {p0, p1, p2};
+    std::vector<int> indices = {0,1,2};
+    triangleMesh mesh = triangleMesh(tri_vertices, indices, 1);
+    triangle tri(&mesh, 0);
     assert(tri.area(p0, p1, p2) == 0.5);
     std::cout << "test_area passed!\n";
 }
@@ -16,8 +20,10 @@ void test_intersection() {
     vec3h p0(0, 0, 0, 1);
     vec3h p1(1, 0, 0, 1);
     vec3h p2(0, 1, 0, 1);
-    std::shared_ptr<bxdf> material = std::make_shared<diffuseBXDF>(color(0.5, 0.5, 0.5, 0));
-    triangle tri(p0, p1, p2, material);
+    std::vector<vec3h> tri_vertices = {p0, p1, p2};
+    std::vector<int> indices = {0,1,2};
+    triangleMesh mesh = triangleMesh(tri_vertices, indices, 1);
+    triangle tri(&mesh, 0);
     
     ray r(vec3h(0.25, 0.25, -1, 1), vec3h(0, 0, 1, 0));
     interval ray_t(0.001, 100.0);
@@ -31,8 +37,10 @@ void test_no_intersection() {
     vec3h p0(0, 0, 0, 1);
     vec3h p1(1, 0, 0, 1);
     vec3h p2(0, 1, 0, 1);
-    std::shared_ptr<bxdf> material = std::make_shared<diffuseBXDF>(color(0.5, 0.5, 0.5, 0));
-    triangle tri(p0, p1, p2, material);
+    std::vector<vec3h> tri_vertices = {p0, p1, p2};
+    std::vector<int> indices = {0,1,2};
+    triangleMesh mesh = triangleMesh(tri_vertices, indices, 1);
+    triangle tri(&mesh, 0);
     
     ray r(vec3h(2, 2, -1, 1), vec3h(0, 0, 1, 0));
     interval ray_t(0.001, 100.0);
@@ -47,8 +55,10 @@ void test_dist() {
     vec3h p1(3, 1, 0, 1);
     vec3h p2(3, 0, 1, 1);
     // basically a triangle at x = 3, parallel to the yz plane
-    std::shared_ptr<bxdf> material = std::make_shared<diffuseBXDF>(color(0.5, 0.5, 0.5, 0));
-    triangle tri(p0, p1, p2, material);
+    std::vector<vec3h> tri_vertices = {p0, p1, p2};
+    std::vector<int> indices = {0,1,2};
+    triangleMesh mesh = triangleMesh(tri_vertices, indices, 1);
+    triangle tri(&mesh, 0);
     
     ray r(vec3h(-1, 0.5, 0, 1), vec3h(1, 0, 0, 0));
     interval ray_t(0.001, 100.0);
@@ -65,7 +75,10 @@ void test_permutation() {
     vec3h p0(3, 0, -1, 1);
     vec3h p1(3, 1, 0, 1);
     vec3h p2(3, 0, 1, 1);
-    triangle tri(p0, p1, p2);
+    std::vector<vec3h> tri_vertices = {p0, p1, p2};
+    std::vector<int> indices = {0,1,2};
+    triangleMesh mesh = triangleMesh(tri_vertices, indices, 1);
+    triangle tri(&mesh, 0);
 
     // Test case where direction is along the x-axis
     vec3h direction1(1.0, 0.0, 0.0, 0.0);
